2fdf3f5206d9ea23a171b77bcaf2db16218322fe
shivani.singh <shivani.singh@nihilent.com>
2020-05-19 17:19:56 +0530

    rebase done

--- App.js
+++ App.js
@@ -29,7 +29,26 @@ const onClickButton = () => {
 
 }
   return (
-   <Registration/>
+    <View style={styles.container}>
+      <TextInput style={styles.TextInput} placeholder = 'Enter message' onChangeText={(val) => {setTextValue(val)} }/>
+      <Button title='Submit' onPress={onClickButton}/>
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  TextInput: {
+    marginBottom: 20,
+    borderWidth: 1,
+    padding: 5,
+    width: 200
+  }
+
+});
