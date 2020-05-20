26ea136aba7e01c72549f5bc15da26e9b2c0f3d1
shivani.singh <shivani.singh@nihilent.com>
2020-05-20 13:59:50 +0530

    removed delete files

--- App.js
+++ App.js
@@ -1,37 +1,36 @@
-import React,{useState} from 'react';
-import {decode, encode} from 'base-64'
-if (!global.btoa) {  global.btoa = encode }
+import { decode, encode } from 'base-64';
+if (!global.btoa) { global.btoa = encode }
 if (!global.atob) { global.atob = decode }
-import { StyleSheet, Text, View ,TextInput,Button} from 'react-native';
+
+import React, { useState } from 'react';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
 import { db } from './Enviroment/FirebaseConfig';
-import Registration from './Screens/Registration';
 
 
 
 export default function App() {
-  const [textValue,setTextValue] = useState()
   
-const onClickButton = () => {
- 
-  db.collection("users").add({
-    firstName: "Shivani",
-    lastName: "Singh",
-    role: "Nss",
-    email:"Shivani.Singh@nihilent.com"
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-    console.error("Error adding document: ", error);
-});
+ const onClickButton = () => {
+
+ const ref=db.collection("users")
+    ref.onSnapshot(querySnapshot=>{
+     
+      querySnapshot.forEach(doc=>{
+       
+        const{firstName,lastName,password,role,email}=doc.data();
+        // console.log(firstName,lastName,password,email,role);
+
+       
+        
+        })
+    })
 
 
-}
+  }
   return (
     <View style={styles.container}>
-      <TextInput style={styles.TextInput} placeholder = 'Enter message' onChangeText={(val) => {setTextValue(val)} }/>
-      <Button title='Submit' onPress={onClickButton}/>
+      <TextInput style={styles.TextInput} placeholder='Enter message' onChangeText={(val) => { setTextValue(val) }} />
+      <Button title='Submit' onPress={onClickButton} />
     </View>
   );
 }
--- GlobalStyle/GlobalStyles.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet} from 'react-native';
-
-export const globalStyles = StyleSheet.create({
-    container:{
-        flex:1,
-        padding:30,
-    },
-});
