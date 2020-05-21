ae24d1c2ede8002b2cab8bc8f84666dd64926803
shivani.singh <shivani.singh@nihilent.com>
2020-05-21 12:51:42 +0530

    Implemented login functionality with firebase

--- App.js
+++ App.js
@@ -1,54 +1,16 @@
 import { decode, encode } from 'base-64';
+import React from 'react';
+import Navigator from './routes/LoginStack';
 if (!global.btoa) { global.btoa = encode }
 if (!global.atob) { global.atob = decode }
-
-import React, { useState } from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
-import { db } from './Enviroment/FirebaseConfig';
 
 
 
 export default function App() {
-  
- const onClickButton = () => {
 
- const ref=db.collection("users")
-    ref.onSnapshot(querySnapshot=>{
-     
-      querySnapshot.forEach(doc=>{
-       
-        const{firstName,lastName,password,role,email}=doc.data();
-        // console.log(firstName,lastName,password,email,role);
-
-       
-        
-        })
-    })
-
-
-  }
   return (
-    <View style={styles.container}>
-      <TextInput style={styles.TextInput} placeholder='Enter message' onChangeText={(val) => { setTextValue(val) }} />
-      <Button title='Submit' onPress={onClickButton} />
-    </View>
+    <Navigator />
   );
 }
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  TextInput: {
-    marginBottom: 20,
-    borderWidth: 1,
-    padding: 5,
-    width: 200
-  }
-
-});
